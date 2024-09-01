57e5b54dad94026e1e9523ecbfd197b6e4065268
Atanu Khamrui <atanukhamrui2002@gmail.com>
2024-09-02 01:33:24 +0530

    first commit

---
+++ longest_substr.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string str;
+    cin >> str;
+
+    vector<string> arr;
+    string temp;
+    stringstream ss(str);
+
+    while(getline(ss, temp, ',')){
+        arr.push_back(temp);
+    }
+
+    int maxLen = 0;
+
+    for(const auto& s : arr){
+        if(s.length() > maxLen){
+            maxLen = s.length();
+        }
+    }
+
+    cout << maxLen;
+
+    return 0;
+}

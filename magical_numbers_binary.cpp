5a810496b1bd39e7adf9e237f82d014ded8d51a3
Atanu Khamrui <atanukhamrui2002@gmail.com>
2024-09-06 01:00:57 +0530

    first commit

--- magical_numbers_binary.cpp
+++ magical_numbers_binary.cpp
@@ -3,16 +3,24 @@ using namespace std;
 
 int countMagicalNum(int N) {
     int count = 0;
-    for(int i=1; i<=N; i++){
+    
+    // Loop through all numbers from 1 to N
+    for (int i = 1; i <= N; i++) {
+        int num = i; // Preserve the original number
         int binSum = 0;
-        while(i > 0){
-            int lBinDig = i % 2;
-            if(lBinDig == 0) binSum += 1;
-            if(lBinDig == 1) binSum += 2;
-            i = i/2;
+
+        // Calculate the binary sum according to your rule
+        while (num > 0) {
+            int lBinDig = num % 2;
+            if (lBinDig == 0) binSum += 1;
+            if (lBinDig == 1) binSum += 2;
+            num = num / 2;
         }
-        if(binSum % 2 != 0) count++;
+
+        // If the binary sum is odd, increment the count
+        if (binSum % 2 != 0) count++;
     }
+
     return count;
 }
 

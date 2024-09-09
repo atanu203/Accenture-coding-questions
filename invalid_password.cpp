bfc3a1645976f0cc0ba3261ce58e8caa13d9f085
Atanu Khamrui <atanukhamrui2002@gmail.com>
2024-09-09 12:31:24 +0530

    code fixed

--- invalid_password.cpp
+++ invalid_password.cpp
@@ -1,37 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
 bool validPassword(string str) {
     int ln = str.length();
-
-    if(ln < 4) return false;
-
-    if(str[0] - '0' >= 0 && str[0] - '0' <= 9) return false;
-
     int cap = 0, numeric = 0;
 
-    for(int i=1; i<ln; i++){
+    if(ln < 4) return false;
+    if(isdigit(str[0])) return false;
 
+    for(int i=0; i<ln; i++){
         if(str[i] == ' ' || str[i] == '/') return false;
-
-        if(str[0] - '0' >= 0 && str[0] - '0' <= 9) numeric++;
-
-        if(str[i] >= 'A' && str[i] <= 'Z') cap++;
+        if(isdigit(str[i])) numeric++;
+        if(isupper(str[i])) cap++;
     }
 
     return (cap > 0 && numeric > 0);
 }
 
-
 int main() {
-
     string password;
     cin >> password;
 
     bool ans = validPassword(password);
 
-    cout << ans;
+    cout << ans << endl;
 
     return 0;
 }

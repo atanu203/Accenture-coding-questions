04bf20f1ae3b47bd3121c77c39cb2fba2f0ea3be
Atanu Khamrui <atanukhamrui2002@gmail.com>
2024-09-29 00:43:59 +0530

    first commit

--- Integer_break.cpp
+++ Integer_break.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int breakInteger(int num) {
-    if (num <= 3) return num - 1;
-    int quotient = num / 3, remainder = num % 3;
-    if (remainder == 0) return pow(3, quotient);
-    if (remainder == 1) return pow(3, quotient - 1) * 4;
+    if(num <= 3) return num - 1;
+
+    int quotient = num / 3;
+    int remainder = num % 3;
+
+    if(remainder == 0){
+        return pow(3, quotient);
+    }
+    else if(remainder == 1){
+        return pow(3, quotient - 1) * 4;
+    }
+    
     return pow(3, quotient) * 2;
 }
 
